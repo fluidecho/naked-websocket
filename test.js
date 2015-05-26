4178777c2116d673125553d7cf78cf1adafd8cb7
markcode <fluidecho@gmail.com>
2015-05-26 16:22:11 +1000

    test script

---
+++ test.js
@@ -0,0 +1,31 @@
+var nws = require('./');
+var assert = require('assert');
+
+var closed;
+
+nws.createServer({protocol: 'ws'}, function(sser) {
+  sser.on('data', function(chunk) {
+    chunk = chunk.toString();
+    console.log(chunk);
+    assert.equal(chunk, 'k');
+    closed = true;
+    sser.end();
+  });
+  setTimeout(function(){
+    sser.write('o');
+  }, 50);   
+}).listen(8585, function() { 
+  nws.connect({protocol: 'ws', hostname: '127.0.0.1', port: 8585}, function(sclt) {
+    sclt.on('data', function(chunk) {
+        chunk = chunk.toString();
+        console.log(chunk);
+        assert.equal(chunk, 'o');
+        sclt.write('k');
+    });
+    sclt.on('close', function() {
+      assert(closed);
+      process.exit(0);
+    });
+  });
+});
+
